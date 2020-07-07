c56cdc4e738d5b26c9ff14d1145637087f852cf6
abhay-ranawat <52895994+abhay-ranawat@users.noreply.github.com>
2020-07-08 05:17:51 +0530

    Initial Code

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://strapi-s.herokuapp.com/',
+  url: 'https://strapijs.glitch.me/',
   admin: {
       url: '/',
       serverAdminPanel: false
